2a3ae2f88c7dfe747614b43411ec8410a024ddf8
tezerabe <88900356+tezerabe@users.noreply.github.com>
2021-08-18 17:08:26 -0700

    emploeye ifnor class addedd

---
+++ Class-of-August21/src/com/craft/Employeeinfo.java
@@ -0,0 +1,13 @@
+package com.craft;
+
+public class Employeeinfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		string firstname= "Jami"; 
+		string lastname = "kebe"		
+		System.out.println("employee full name:" + firstname + " " + lastname);
+	}
+
+}
